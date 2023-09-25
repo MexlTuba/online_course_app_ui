7d7c15b0d17deedd3bdc23f9b43dc6b556364d1c
Mexl Tuba <mexldelvertuba@gmail.com>
2023-09-25 14:29:27 +0800

    ignores consts

--- lib/home_free_cards/first_flutter_dev.dart
+++ lib/home_free_cards/first_flutter_dev.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:online_course_app_ui/popup_pages/popup_graphic_design.dart';
 
